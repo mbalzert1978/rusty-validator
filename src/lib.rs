699f0ead19109c0d7b2e21656a1ed1134d7a51fa
Markus Iorio <markus.iorio@bfd.de>
2024-06-18 09:34:01 +0200

    rust lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,53 +2,13 @@ use pyo3::prelude::*;
 use validator::ValidateEmail;
 
 #[pyfunction]
-fn email_validator(email: String) -> PyResult<bool> {
+fn validate(email: String) -> PyResult<bool> {
     Ok(email.validate_email())
 }
 
 #[pymodule]
-fn validate_email(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(email_validator, m)?)?;
+fn _email_validator(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_function(wrap_pyfunction!(validate, m)?)?;
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pyo3::types::IntoPyDict;
-    use pyo3::Python;
-
-    #[test]
-    fn test_valid_email() {
-        pyo3::prepare_freethreaded_python();
-        Python::with_gil(|py| {
-            let validate_email = PyModule::new(py, "validate_email").unwrap();
-            validate_email.add_function(wrap_pyfunction!(email_validator, validate_email).unwrap()).unwrap();
-
-            let locals = [("validate_email", validate_email)].into_py_dict(py);
-            let result: bool = py
-                .eval("validate_email.email_validator('test@example.com')", None, Some(&locals))
-                .unwrap()
-                .extract()
-                .unwrap();
-            assert!(result);
-        });
-    }
-
-    #[test]
-    fn test_invalid_email() {
-        pyo3::prepare_freethreaded_python();
-        Python::with_gil(|py| {
-            let validate_email = PyModule::new(py, "validate_email").unwrap();
-            validate_email.add_function(wrap_pyfunction!(email_validator, validate_email).unwrap()).unwrap();
-
-            let locals = [("validate_email", validate_email)].into_py_dict(py);
-            let result: bool = py
-                .eval("validate_email.email_validator('invalid-email')", None, Some(&locals))
-                .unwrap()
-                .extract()
-                .unwrap();
-            assert!(!result);
-        });
-    }
 }
