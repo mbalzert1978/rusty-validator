7ef401d281db2d56bd46696fc286dd0ccd7693fa
Markus Iorio <mbalzert@gmail.com>
2024-06-18 20:03:24 +0200

    rust expose more of the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
 use pyo3::prelude::*;
 use validator::ValidateEmail;
+use validator::ValidateIp;
+use validator::ValidateUrl;
 
 #[pyfunction]
-fn validate(email: String) -> PyResult<bool> {
+fn validate_email(email: String) -> PyResult<bool> {
     Ok(email.validate_email())
 }
 
+#[pyfunction]
+fn validate_url(url: String) -> PyResult<bool> {
+    Ok(url.validate_url())
+}
+
+#[pyfunction]
+fn validate_ip(ip: String) -> PyResult<bool> {
+    Ok(ip.validate_ip())
+}
+
 #[pymodule]
-fn _email_validator(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _validator(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_function(wrap_pyfunction!(validate, m)?)?;
+    m.add_function(wrap_pyfunction!(validate_email, m)?)?;
+    m.add_function(wrap_pyfunction!(validate_url, m)?)?;
+    m.add_function(wrap_pyfunction!(validate_ip, m)?)?;
     Ok(())
 }
 
@@ -23,13 +37,13 @@ mod tests {
     fn test_valid_email() {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let email_validator = PyModule::new_bound(py, "_email_validator").unwrap();
-            _email_validator(&email_validator).unwrap();
+            let rusty_validator = PyModule::new_bound(py, "_validator").unwrap();
+            _validator(&rusty_validator).unwrap();
 
-            let locals = [("email_validator", email_validator)].into_py_dict_bound(py);
+            let locals = [("rusty_validator", rusty_validator)].into_py_dict_bound(py);
             let result: bool = py
                 .eval_bound(
-                    "email_validator.validate('example@example.com')",
+                    "rusty_validator.validate_email('example@example.com')",
                     None,
                     Some(&locals),
                 )
@@ -44,13 +58,13 @@ mod tests {
     fn test_invalid_email() {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let email_validator = PyModule::new_bound(py, "_email_validator").unwrap();
-            _email_validator(&email_validator).unwrap();
+            let rusty_validator = PyModule::new_bound(py, "_validator").unwrap();
+            _validator(&rusty_validator).unwrap();
 
-            let locals = [("email_validator", email_validator)].into_py_dict_bound(py);
+            let locals = [("rusty_validator", rusty_validator)].into_py_dict_bound(py);
             let result: bool = py
                 .eval_bound(
-                    "email_validator.validate('invalid-email')",
+                    "rusty_validator.validate_email('invalid-email')",
                     None,
                     Some(&locals),
                 )
